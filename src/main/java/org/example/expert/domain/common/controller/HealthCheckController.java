78e7574ba631b72b31621a510a01cbd6ba53a971
iboyeon0816 <iboyeon0816@gmail.com>
2025-03-14 19:40:59 +0900

    fix(3-3): CrossOrigin 어노테이션 제거

--- src/main/java/org/example/expert/domain/common/controller/HealthCheckController.java
+++ src/main/java/org/example/expert/domain/common/controller/HealthCheckController.java
@@ -1,7 +1,6 @@
 package org.example.expert.domain.common.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +13,6 @@ import java.util.Map;
 public class HealthCheckController {
 
     @GetMapping
-    @CrossOrigin(origins = "*")
     public ResponseEntity<Map<String, String>> health() {
         Map<String, String> response = new HashMap<>();
         response.put("status", "UP");
